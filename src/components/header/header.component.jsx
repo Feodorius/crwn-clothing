953af8a079fac06512f46baddba02507db4c3073
Fedor Kaputsky <fedor_kaputsky@epam.com>
2021-09-06 00:22:53 +0300

    feat: add redux for cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' />
@@ -30,18 +32,22 @@ const Header = ({ currentUser }) => (
                         SIGN OUT
                     </div>
                     :
-                    <Link
+                    (<Link
                         to='/signin'
                         className='option'>
                         SIGN IN
-                    </Link>
+                    </Link>)
             }
+            <CartIcon />
         </div>
+        {!hidden && <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = (
+    { user: { currentUser }, cart: { hidden } }) => ({
+        currentUser,
+        hidden
+    })
 
 export default connect(mapStateToProps)(Header);
